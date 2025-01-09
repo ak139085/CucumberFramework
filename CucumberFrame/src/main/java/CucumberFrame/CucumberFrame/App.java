741f0ddefdd810d9e75cc2e9d3ffff84f6b89a2e
Ankit <ak139085@gmail.com>
2025-01-09 20:19:42 +0530

    First -Commit

---
+++ CucumberFrame/src/main/java/CucumberFrame/CucumberFrame/App.java
@@ -0,0 +1,10 @@
+package CucumberFrame.CucumberFrame;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
